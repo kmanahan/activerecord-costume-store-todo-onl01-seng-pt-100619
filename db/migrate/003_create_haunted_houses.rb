8ef3974c1b036ba8b08670b73c44c50a5cc411f5
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-11 13:24:56 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,8 +7,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :theme
       t.integer :price 
       t.boolean :family_friendly 
-      t.integer :opening_date
-      t.integer :closing_date 
+      t.datetime :opening_date
+      t.datetime :closing_date 
       t.text :description
     end
   end
